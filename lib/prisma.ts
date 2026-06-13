0f4edc8f8877b1276bc84bc9628045d571a354a8
nisthajain12 <nisthajain2812@gmail.com>
2026-06-13 22:00:41 +0530

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
 })
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require('@prisma/client')
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  prisma: any
 }
 
 export const prisma =
