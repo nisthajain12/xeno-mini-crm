73dd09a336b9f07f667f25a31425739b6a127f98
nisthajain12 <nisthajain2812@gmail.com>
2026-06-14 23:43:41 +0530

    fix: prisma client import and nixpacks build order

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,17 @@
 import 'dotenv/config'
 import { PrismaNeon } from '@prisma/adapter-neon'
-
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-})
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  prisma: any
+  prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ adapter })
+function createPrismaClient() {
+  const connectionString = process.env.DATABASE_URL!
+  const adapter = new PrismaNeon({ connectionString })
+  return new PrismaClient({ adapter } as ConstructorParameters<typeof PrismaClient>[0])
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
